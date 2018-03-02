8ab88281ba36741b11d393e60a0a8b3e58004b48
Edu Saborit <edu@barkibu.com>
2018-03-02 22:44:41 +0100

    Seed ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create([
+  { name: 'Tomaca' },
+  { name: 'Bajoqueta' },
+  { name: 'Cuixa de pollastre' },
+  { name: 'Garrofons' },
+  { name: 'Caragol bavós' },
+  { name: 'Conill' },
+  { name: 'Carxofa' }
+])
+
+Ingredient.find_each do |ingredient|
+  Resource.create(item: ingredient)
+end
